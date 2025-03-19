a6776077d45b4f1d269a1997d8e5f247abf59f09
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-19 14:17:33 -0300

    Lançamento de Dados

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Jogo dos Dados");
+                Console.WriteLine("---------------------");
+
+                Console.Write("Pressione ENTER para lançar um dado");
+                Console.ReadLine();
+
+                Random geradordenumerosaleatorios = new Random();
+                int resultado = geradordenumerosaleatorios.Next(1, 7);
+
+                Console.WriteLine("---------------------");
+                Console.WriteLine($"O valor sorteado foi {resultado}!");
+                Console.WriteLine("---------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.WriteLine("Deseja Continuar? (S/N)");
+                string Continuar = Console.ReadLine()!.ToUpper();
+
+                if (Continuar != "S")
+                    break;
+
+            }
         }
     }
 }
